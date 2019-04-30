e46d8f7b92f9b5851c24aeb2c792ce6dc2c03514
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-04-30 22:50:10 +0100

    Output CSV + minor gitignore changes

--- csv_reproject/csv_proj_writer.py
+++ csv_reproject/csv_proj_writer.py
@@ -1,3 +1,4 @@
+import csv
 from typing import TextIO, List
 
 
@@ -12,11 +13,17 @@ class CsvProjWriter:
         self.file = file
         self.to_x_header = to_x_header
         self.to_y_header = to_y_header
+        self.writer = csv.writer(
+            self.file,
+            delimiter=',',
+            quotechar='"',
+            quoting=csv.QUOTE_MINIMAL
+        )
 
     def write_headers(self, headers: List[str]):
         headers.extend([self.to_x_header, self.to_y_header])
-        print(headers)
+        self.writer.writerow(headers)
 
     def write_row(self, row: List[str], x: float, y: float):
         row.extend([str(x), str(y)])
-        print(row)
+        self.writer.writerow(row)
